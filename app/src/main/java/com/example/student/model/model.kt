e0c91f5b3d245cfefa0b672ef122feb09af842c1
bryandanendra <bryan4icm@gmail.com>
2025-03-18 07:54:23 +0700

    first commit

---
+++ app/src/main/java/com/example/student/model/model.kt
@@ -0,0 +1,9 @@
+package com.example.student.model
+
+data class Student(
+    var id:String?,
+    var name:String?,
+    var bod:String?,
+    var phone:String?,
+    var photoUrl:String?
+)
---
+++ app/src/main/java/com/example/student/view/StudentListFragment.kt
@@ -0,0 +1,23 @@
+package com.example.student.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.student.R
+import com.example.student.databinding.FragmentStudentListBinding
+
+
+class StudentListFragment : Fragment() {
+    private lateinit var binding:FragmentStudentListBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentStudentListBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+    
+}
